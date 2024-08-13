9338096b12d346d60965cd872532d50cf085475c
VidhiUndavia <97037845+VidhiUndavia@users.noreply.github.com>
2024-08-13 08:48:41 +0530

    code

---
+++ while1.dart
@@ -0,0 +1,7 @@
+void main() {
+  var start = 1, end = 50;
+  while (start <= end) {
+    print(start);
+    start++;
+  }
+}
---
+++ while2.dart
@@ -0,0 +1,7 @@
+void main() {
+   var start = 50, end = 1;
+  while (start >= end) {
+    print(start);
+    start--;
+  }
+}
